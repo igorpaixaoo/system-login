5c91aa3d11942861e4a92d9339d55a6897ec62a7
= <igorsantid6@gmail.com>
2026-01-06 21:49:25 -0300

    resolvendo problema

--- src/scripts/Register.js
+++ src/scripts/Register.js
@@ -1,7 +1,10 @@
 document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault(); // 👈 trava o reload
+  e.preventDefault(); 
 
 });
+
+const dadosString = localStorage.getItem("Dados");
+const dados = JSON.parse(dadosString);
 
 const url = "http://localhost:8080/user/create"
 
@@ -11,23 +14,40 @@ async function apiRegister() {
     var password = document.getElementById('password').value
     var nome = document.getElementById('nome').value
     var sobrenome = document.getElementById('sobrenome').value
-    var endereco = document.getElementById('endereco').value
-    
-    const post = await fetch(url, {
-        method: "POST", 
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email,
-            senha: password,
-            nome: nome,
-            sobrenome: sobrenome,
-            endereco: endereco
+    var endereco = document.getElementById('address').value
+    const userError = document.getElementById('erroUser')
+
+    if(email != dados.email || password != dados.senha){
+        const post = await fetch(url, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                senha: password,
+                nome: nome,
+                sobrenome: sobrenome,
+                endereco: endereco
+            })
+
         })
 
-    })
+        const result = await post.json()
+        console.log(result)
 
-    const result = await post.json()
-    console.log(result)
+        userError.style.display = 'none'
+
+        window.location.href = "../pages/login.html"       
+    }else{
+        userError.style.display = 'block'
+    } 
+    
+
+
+
+
+
+
+
 }
